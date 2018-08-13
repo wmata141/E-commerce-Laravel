d79dfffd13ab2f9f52ceec27612777a5829e51c5
William Mata <wmata0141@gmail.com>
2018-08-13 19:32:15 -0400

    commit 10

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Http\Requests;
+use Session;
+use Illuminate\support\Facades\Redirect;
+
+session_start();
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin_login');
+    }
+
+    public function show_dashboard()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function dashboard(Request $request)
+    {
+        $admin_email = $request->admin_email;
+        $admin_password = md5($request->admin_password);
+        $result = DB::table('tbl_admin')
+                    ->where('admin_email',$admin_email)
+                    ->where('admin_password',$admin_password)
+                    ->first();
+
+        if($result) {
+            Session::put('admin_name',$result->admin_name);
+            Session::put('admin_id',$result->admin_id);
+            return Redirect::to('/dashboard');
+        } else {
+            Session::put('messege','Email or Password Invalid');
+            return Redirect::to('/admin');
+        }
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
